83b14f37e4718ad26cf21160fc016662107b40b5
ty1972873004 <1972873004@qq.com>
2018-05-04 10:09:28 +0800

    缓存修改

--- tpay-common-cache/src/main/java/com/tpay/cache/redis/CacheUtil.java
+++ tpay-common-cache/src/main/java/com/tpay/cache/redis/CacheUtil.java
@@ -1,11 +1,9 @@
 package com.tpay.cache.redis;
 
-import com.tpay.cache.redis.impl.JedisClientSingleImpl;
+import com.tpay.common.utils.SpringContextUtil;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * @author tuyong
@@ -13,21 +11,17 @@ import org.springframework.context.annotation.Configuration;
  * @DESCRIPTION redis锁工具类
  * @create 2017-06-15 14:14
  **/
-@Configuration
 public class CacheUtil {
     private static Logger logger = LoggerFactory.getLogger(CacheUtil.class);
 
     private static JedisClient jedisClient;
 
-    private static JedisClientSingleImpl jedisClientSingleImpl;
 
-
-    @Bean
     public static JedisClient getCache() {
         if (jedisClient == null) {
             synchronized (CacheUtil.class) {
-                if (jedisClient == null) {
-                    jedisClient = new JedisClientSingleImpl();
+                if(jedisClient==null) {
+                    jedisClient = (JedisClient) SpringContextUtil.getBean("jedisClient");
                 }
             }
         }
@@ -35,27 +29,12 @@ public class CacheUtil {
     }
 
 
-    @Bean
-    public JedisClientSingleImpl setRedisHelper() {
-        jedisClientSingleImpl = getJedisClientSingle();
-        return jedisClientSingleImpl;
-    }
-
-    public static JedisClientSingleImpl getJedisClientSingle() {
-        if (jedisClientSingleImpl == null) {
-            synchronized (CacheUtil.class) {
-                if (jedisClientSingleImpl == null) {
-                    jedisClientSingleImpl = new JedisClientSingleImpl();
-                }
-            }
-        }
-        return jedisClientSingleImpl;
-    }
-
-
     public  static boolean getLock(String key) {
+        if(jedisClient==null) {
+            jedisClient = (JedisClient) SpringContextUtil.getBean("jedisClient");
+        }
         try {
-            if (!getJedisClientSingle().exists(key)) {
+            if (!jedisClient.exists(key)) {
                 synchronized (CacheUtil.class) {
                     if (!jedisClient.exists(key)) {
                         if (jedisClient.setnx(key, String.valueOf(System.currentTimeMillis())) == 1) {
